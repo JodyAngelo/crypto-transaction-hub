fdd4dac3524873a3bcf7b3ca188c67ff71902226
JodyAngelo <jody.angelo18@gmail.com>
2025-10-22 21:17:55 +0700

    feat: add Transactions smart contract for transfer recording

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Transactions {
+    uint256 transcationCount;
+
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct {
+        address sender; 
+        address receiver; 
+        uint amount; 
+        string message; 
+        uint256 timestamp; 
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+    function createTransaction(address payable receiver, uint amount, string memory keyword, string memory message) public {
+        transcationCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+        return transactions;
+    }
+}
